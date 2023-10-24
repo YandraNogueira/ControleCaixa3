17311bf3f639bf4ffd3966a13d1a7397c9af704e
YandraNogueira <100050061+YandraNogueira@users.noreply.github.com>
2023-10-24 14:17:52 -0300

    Delete preencher-um-select directory

--- preencher-um-select/conect.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$conexao = new PDO("mysql:dbname=controle; host=localhost;charset=utf8","root","");
-
-
-
--- preencher-um-select/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "";
-	$dbname = "controle";
-	
-	//Criar a conexão
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-?>
